1b300d4bb452efb87c61b5878b13a0ec8ff1bbe0
Khondoker  Tanvir Hossain <tanvir@cellosco.pe>
2024-07-17 16:51:58 +0600

    db properties update

--- src/main/java/com/tanvir/MaxLiveSpringHomeApplication.java
+++ src/main/java/com/tanvir/MaxLiveSpringHomeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StateMachineServiceApplication {
+public class MaxLiveSpringHomeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StateMachineServiceApplication.class, args);
+		SpringApplication.run(MaxLiveSpringHomeApplication.class, args);
 	}
 
 }
