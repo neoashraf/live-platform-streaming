f977f6e3ab03120c1ea1fad4b15d63e668024d73
heronmoy <heronmoy.cuet.eee@gmail.com>
2024-10-07 12:06:20 +0600

    firebase configs shifted to application properties

--- src/main/java/com/tanvir/core/config/FirebaseConfig.java
+++ src/main/java/com/tanvir/core/config/FirebaseConfig.java
@@ -6,6 +6,7 @@ import com.google.firebase.FirebaseOptions;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
@@ -15,13 +16,19 @@ import java.io.IOException;
 @Configuration
 public class FirebaseConfig {
 
+    @Value("${firebase.credentials.file}")
+    private String firebaseCredentialsPath;
+
+    @Value("${firebase.database.url}")
+    private String firebaseDatabaseUrl;
+
     @PostConstruct
     public void initialize() {
         try {
-            GoogleCredentials credentials = GoogleCredentials.fromStream(new ClassPathResource("max-live-1b02e-firebase-adminsdk-mhscw-2609c6ffbe.json").getInputStream());
+            GoogleCredentials credentials = GoogleCredentials.fromStream(new ClassPathResource(firebaseCredentialsPath).getInputStream());
             FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(credentials)
-                .setDatabaseUrl("https://max-live-1b02e-default-rtdb.europe-west1.firebasedatabase.app/")
+                .setDatabaseUrl(firebaseDatabaseUrl)
                 .build();
 
             if (FirebaseApp.getApps().isEmpty()) {
