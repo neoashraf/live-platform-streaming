2a2cd061c2e54505e4af3f14a65e157025bb7268
heronmoy <heronmoy.cuet.eee@gmail.com>
2024-10-24 02:50:54 +0600

    api: liveroom summary; test : Green

--- src/main/java/com/tanvir/features/liveroomsummary/application/service/LiveRoomSummaryService.java
+++ src/main/java/com/tanvir/features/liveroomsummary/application/service/LiveRoomSummaryService.java
@@ -9,15 +9,18 @@ import com.tanvir.features.liveroomsummary.domain.LiveRoomSummary;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.security.web.reactive.result.method.annotation.CurrentSecurityContextArgumentResolver;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -34,6 +37,7 @@ public class LiveRoomSummaryService implements LiveRoomSummaryUseCase {
     private LiveRoomSummaryPersistencePort port;
 
     private final ModelMapper modelMapper;
+    private CurrentSecurityContextArgumentResolver reactiveCurrentSecurityContextArgumentResolver;
 
     public LiveRoomSummaryService(ModelMapper modelMapper) {
         this.modelMapper = modelMapper;
@@ -43,85 +47,95 @@ public class LiveRoomSummaryService implements LiveRoomSummaryUseCase {
     @Override
     public Mono<LiveRoomSummary> processLiveRoomSummary(LiveRoom liveRoom) {
         String userId = liveRoom.getUserId();
-        long durationInSeconds = liveRoom.getDurationInSeconds();
+//        long durationInSeconds = 4000; // Example duration, you may uncomment the next line if needed
+         long durationInSeconds = liveRoom.getDurationInSeconds();
 
-        // Determine the current date based on UTC 1 AM rule
-        LocalDateTime currentTime =LocalDateTime.now().atOffset(ZoneOffset.UTC).toLocalDateTime();
-        LocalDate currentDate = LocalDateTime.now().atOffset(ZoneOffset.UTC).toLocalDate();
-        LocalDateTime liveRoomCreationTime = liveRoom.getCreatedOn().atOffset(ZoneOffset.UTC).toLocalDateTime();
+        ZonedDateTime currentUTC = ZonedDateTime.now(ZoneOffset.UTC);
+        /*ZonedDateTime currentUTC = ZonedDateTime.of(
+                2025, 1, 2, // Year, Month, Day
+                1, 0, 0, 0, // Hour (1 AM), Minute, Second, Nanosecond
+                ZoneOffset.UTC // Time zone offset (UTC)
+        );*/
+        log.info("Current UTC time: {}", currentUTC);
 
-        log.info("currentTime : {}", currentTime);
-        log.info("currentDate : {}", currentDate);
-        log.info("liveRoomCreationTime : {}", liveRoomCreationTime);
-        if (liveRoomCreationTime.isBefore(currentDate.atTime(7, 0))) {
-            log.info("Before 1 AM UTC, set to the previous day");
-            currentDate = currentDate.minusDays(1); // Before 1 AM UTC, set to the previous day
+        // Determine the day end time for logic
+        ZonedDateTime dayEndTime = currentUTC.withHour(1).withMinute(0).withSecond(0).withNano(0);
+
+        // Adjust current date based on the time
+        if (currentUTC.isBefore(dayEndTime)) {
+            log.info("Current UTC time is before 1 AM");
+            currentUTC = currentUTC.minusDays(1); // Use the previous day
         }
+
+        // Set the date to check against
+        String dateToCheck = currentUTC.toLocalDate().toString();
 
         // Query for existing LiveRoomSummaryEntity by userId and date
         Query query = new Query();
         query.addCriteria(Criteria.where("userId").is(userId)
-                .and("date").is(currentDate.toString()));
+                .and("date").is(dateToCheck));
 
-        // Find the existing summary and update or create a new one reactively
-        LocalDate finalCurrentDate = currentDate;
-        return reactiveMongoTemplate.findOne(query, LiveRoomSummaryEntity.class)
-                .flatMap(summary -> {
-                    log.info("Found existing summary for user : {}", userId);
-                    // Update existing fields
-                    summary.setTotalDuration(summary.getTotalDuration() + durationInSeconds);
-                    summary.setTotalSessions(summary.getTotalSessions() + 1);
+        // Get the latest entry (sorted by createdOn in descending order)
+        query.with(Sort.by(Sort.Direction.DESC, "createdOn")).limit(1);
+        Mono<LiveRoomSummaryEntity> latestEntryMono = reactiveMongoTemplate.findOne(query, LiveRoomSummaryEntity.class);
 
-                    // Add new session details
-                    List<LiveRoomSummaryEntity.SessionDetail> sessionDetails = new ArrayList<>(summary.getSessionDetails());
-                    LiveRoomSummaryEntity.SessionDetail newSessionDetail = new LiveRoomSummaryEntity.SessionDetail();
-                    newSessionDetail.setLiveRoomId(liveRoom.getId());
-                    newSessionDetail.setDuration(durationInSeconds);
-                    sessionDetails.add(newSessionDetail);
-                    summary.setSessionDetails(sessionDetails);
 
-                    // Check if dayTime should be set to "Yes"
-                    if (summary.getTotalDuration() >= 3600) {
-                        summary.setDayTime("Yes");
-                    }
+        ZonedDateTime finalCurrentUTC = currentUTC;
+        return latestEntryMono.flatMap(summary -> {
+            log.info("Found existing summary for user : {}", userId);
+            return updateSummary(summary, durationInSeconds, liveRoom);
+        }).switchIfEmpty(Mono.defer(() -> {
+            // No existing summary found; check the conditions to create a new one
+            log.info("Creating a new summary for user : {}", userId);
+            LiveRoomSummaryEntity newSummary = new LiveRoomSummaryEntity();
+            newSummary.setId(UUID.randomUUID().toString());
+            newSummary.setUserId(userId);
+            newSummary.setDate(finalCurrentUTC.toLocalDate().toString());
+            newSummary.setTotalDuration(durationInSeconds);
+            newSummary.setTotalSessions(1);
+            newSummary.setDayTime(durationInSeconds >= 3600 ? "Yes" : "No");
+            newSummary.setMonth(finalCurrentUTC.getMonthValue());
+            newSummary.setYear(finalCurrentUTC.getYear());
 
-                    summary.setUpdatedOn(LocalDateTime.now());
+            // Set session details
+            LiveRoomSummaryEntity.SessionDetail newSessionDetail = new LiveRoomSummaryEntity.SessionDetail();
+            newSessionDetail.setLiveRoomId(liveRoom.getId());
+            newSessionDetail.setDuration(durationInSeconds);
+            newSummary.setSessionDetails(Collections.singletonList(newSessionDetail));
 
-                    // Save the updated summary back to the database
-                    return reactiveMongoTemplate.save(summary)
-                            .doOnNext(liveRoomSummaryEntity -> log.info("Updated summary for user : {}", liveRoomSummaryEntity))
-                            .map(liveRoomSummaryEntity -> modelMapper.map(liveRoomSummaryEntity, LiveRoomSummary.class)); // Return the liveRoom wrapped in Mono
-                })
-                .switchIfEmpty(Mono.defer(() -> {
-                    // Create new LiveRoomSummaryEntity if it does not exist
-                    log.info("Creating a new summary for user : {}", userId);
-                    LiveRoomSummaryEntity newSummary = new LiveRoomSummaryEntity();
-                    newSummary.setId(UUID.randomUUID().toString());
-                    newSummary.setUserId(userId);
-                    newSummary.setDate(finalCurrentDate.toString());
-                    newSummary.setTotalDuration(durationInSeconds);
-                    newSummary.setTotalSessions(1);
-                    newSummary.setDayTime("No");
+            newSummary.setCreatedOn(ZonedDateTime.now(ZoneOffset.UTC).toInstant());
 
-                    // Set session details
-                    LiveRoomSummaryEntity.SessionDetail newSessionDetail = new LiveRoomSummaryEntity.SessionDetail();
-                    newSessionDetail.setLiveRoomId(liveRoom.getId());
-                    newSessionDetail.setDuration(durationInSeconds);
-                    newSummary.setSessionDetails(Collections.singletonList(newSessionDetail));
-
-                    // Set dayTime if duration >= 3600
-                    if (durationInSeconds >= 3600) {
-                        newSummary.setDayTime("Yes");
-                    }
-
-                    newSummary.setCreatedOn(LocalDateTime.now());
-                    newSummary.setUpdatedOn(LocalDateTime.now());
-
-                    // Save the new summary back to the database
-                    return reactiveMongoTemplate.save(newSummary)
-                            .doOnNext(liveRoomSummaryEntity -> log.info("Created summary for user : {}", liveRoomSummaryEntity))
-                            .map(liveRoomSummaryEntity -> modelMapper.map(liveRoomSummaryEntity, LiveRoomSummary.class)); // Return the liveRoomSummaryEntity wrapped in Mono
-                }));
+            // Save the new summary back to the database
+            return reactiveMongoTemplate.save(newSummary)
+                    .doOnNext(liveRoomSummaryEntity -> log.info("Created summary for user : {}", liveRoomSummaryEntity))
+                    .map(liveRoomSummaryEntity -> modelMapper.map(liveRoomSummaryEntity, LiveRoomSummary.class)); // Return the liveRoomSummaryEntity wrapped in Mono
+        }));
     }
+
+    private Mono<LiveRoomSummary> updateSummary(LiveRoomSummaryEntity summary, long durationInSeconds, LiveRoom liveRoom) {
+        summary.setTotalDuration(summary.getTotalDuration() + durationInSeconds);
+        summary.setTotalSessions(summary.getTotalSessions() + 1);
+
+        // Add new session details
+        List<LiveRoomSummaryEntity.SessionDetail> sessionDetails = new ArrayList<>(summary.getSessionDetails());
+        LiveRoomSummaryEntity.SessionDetail newSessionDetail = new LiveRoomSummaryEntity.SessionDetail();
+        newSessionDetail.setLiveRoomId(liveRoom.getId());
+        newSessionDetail.setDuration(durationInSeconds);
+        sessionDetails.add(newSessionDetail);
+        summary.setSessionDetails(sessionDetails);
+
+        // Check if dayTime should be set to "Yes"
+        if (summary.getTotalDuration() >= 3600) {
+            summary.setDayTime("Yes");
+        }
+
+        summary.setUpdatedOn(ZonedDateTime.now(ZoneOffset.UTC).toInstant());
+
+        // Save the updated summary back to the database
+        return reactiveMongoTemplate.save(summary)
+                .doOnNext(liveRoomSummaryEntity -> log.info("Updated summary for user : {}", liveRoomSummaryEntity))
+                .map(liveRoomSummaryEntity -> modelMapper.map(liveRoomSummaryEntity, LiveRoomSummary.class)); // Return the liveRoom wrapped in Mono
+    }
+
 
 }
