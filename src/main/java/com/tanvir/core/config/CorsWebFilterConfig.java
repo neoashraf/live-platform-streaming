4ff61d8c2d814a623145be4713ad277e77fa73df
heronmoy <heronmoy.cuet.eee@gmail.com>
2025-04-29 23:08:55 +0600

    remove cors

--- src/main/java/com/tanvir/core/config/CorsWebFilterConfig.java
+++ src/main/java/com/tanvir/core/config/CorsWebFilterConfig.java
@@ -1,3 +1,4 @@
+/*
 package com.tanvir.core.config;
 
 import org.springframework.context.annotation.Bean;
@@ -22,3 +23,4 @@ public class CorsWebFilterConfig {
         return source;
     }
 }
+*/
--- src/main/java/com/tanvir/core/config/SecurityConfig.java
+++ src/main/java/com/tanvir/core/config/SecurityConfig.java
@@ -1,3 +1,4 @@
+/*
 package com.tanvir.core.config;
 
 import org.springframework.context.annotation.Bean;
@@ -30,3 +31,4 @@ public class SecurityConfig {
 
     }
 }
+*/
